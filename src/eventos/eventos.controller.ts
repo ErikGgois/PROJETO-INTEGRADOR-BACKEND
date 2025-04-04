a6a09ed43338187fdddfa3462458a9fcec2591eb
MicheleKataoka <kataoka442@gmail.com>
2025-04-03 22:05:36 -0300

    criando controller do eventos

--- src/eventos/eventos.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-
-@ApiTags('eventos')
-@Controller('/eventos')
-export class EventosController{
-    constructor (private cIsEventosArmazenados: EventosArmazenados, private HttpService: Httserv)
-}
