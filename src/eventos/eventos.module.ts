3ef4e1deb8cea7755ea9caded281b08d8387ba4b
GiovannePrado <giojaum@gmail.com>
2025-04-06 17:11:15 -0300

    alteração na eventos.modules

--- src/eventos/eventos.module.ts
+++ src/eventos/eventos.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { EventosController } from "./eventos.controller";
+import { EventosArmazenados } from "./eventos.dm";
 
 @Module({
     imports: [],
-    controllers: [],
-    providers:[]
+    controllers: [EventosController],
+    providers:[EventosArmazenados]
 })
 export class EventosModule{}
